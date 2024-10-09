bb9c778058d9ae047334194677496054c45e16da
ibarhiim-kt <contact.ibrahiim@gmail.com>
2024-10-09 18:06:56 +0100

    updated the footer section

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
 
   return (
     <header className='bg-[#3f4046]'>
-      <div className='max-w-[1280px] mx-auto px-6 py-4 flex justify-between items-center'>
+      <div className='max-w-[1216px] mx-auto px-6 py-4 flex justify-between items-center'>
 
         <div className='flex items-center'>
           <Image
