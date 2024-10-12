a2c095a910bfe65256230ddabe62eccab6d1661a
Wania Khan <waniak305@gmail.com>
2024-10-13 01:44:52 +0500

    Pixel perfect pages for T&C, Privacy, Customer Service, B2B

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -54,7 +54,7 @@ const DetailsPage = ({ params }: Props) => {
       bgColor="#070b18"
       />
       <MDXProvider>
-        <div className="max-w-[720px] mb-10 prose-p:text-[16px] prose-p:leading-[24px] max-md:px-4 max-xl:px-10 max-lg:px-4 prose mx-auto mt-16 prose-li:-ml-2.5 prose-li:list-disc prose-ul:text-[#475467] prose-li:text-[#475467] prose-ul:text-[16px] prose-p:text-[#475467] prose-h2:text-[20px] prose-h2:leading-[30px] prose-h2:text-[#101828] prose-ul:leading-[24px] prose-li:marker:text-[24px] prose-a:no-underline prose-li:marker:text-[#475467] prose-li:my-0">
+        <div className="max-w-[720px] mb-10 prose-p:text-[16px] prose-p:leading-[24px] max-md:px-4 max-xl:px-10 max-lg:px-4 prose mx-auto max-md:mt-12 mt-16 prose-li:-ml-2.5 prose-li:list-disc prose-ul:text-[#475467] prose-li:text-[#475467] prose-ul:text-[16px] prose-p:text-[#475467] prose-h2:text-[20px] prose-h2:leading-[30px] prose-h2:text-[#101828] prose-ul:leading-[24px] prose-li:marker:text-[24px] prose-a:no-underline prose-li:marker:text-[#475467] prose-li:my-0">
           <Content />
         </div>
       </MDXProvider>
