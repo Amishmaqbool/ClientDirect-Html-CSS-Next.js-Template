141747ac06ff6c0fa2efd2a9a998aa65713987e2
Amishmaqbool <amishmaqbool@gmail.com>
2024-10-18 23:42:27 +0500

    Loader addition to Redirect

--- src/app/redirect/page.tsx
+++ src/app/redirect/page.tsx
@@ -2,24 +2,35 @@
 import React, {useEffect , Suspense} from "react";
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
+import {ClipLoader} from 'react-spinners';
 //import { time } from "console";
 
 function RedirectAfter() {
     const searchParams = useSearchParams();
     const router = useRouter();
+
     useEffect(() => {
         const params = searchParams.toString();
         console.log("this is params");
+        const timer = setTimeout(() => {
         if (params) {
-        localStorage.setItem("access_token", params);
-        router.push('/');
+            localStorage.setItem("access_token", params);
         }
+        router.push("/"); 
+        }, 7000); 
+
+    return () => clearTimeout(timer);
     }, [router, searchParams]);
 
     return(
     <>
     <div className="max-w-[1200px] flex flex-col justify-center items-center mx-auto">
-        <h1>This is the Redirect Page</h1>
+        <h1 className="text-3xl text-[#111827] mt-8">This is the Redirect Page</h1>
+          <ClipLoader className="mt-3"
+            color={"#111827"} 
+            loading={true}
+            size={40} 
+          />
     </div>
     </>
     );
@@ -31,5 +42,4 @@ export default function Redirect() {
         <RedirectAfter />
       </Suspense>
     );
-  } 
-  
+} 
