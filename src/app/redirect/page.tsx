8cc6dcdd4139fd1dbf83123b0d2de944cbece484
Amishmaqbool <amishmaqbool@gmail.com>
2024-10-18 21:10:18 +0500

    Redirect/blank page on Login/Register

---
+++ src/app/redirect/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React, {useEffect} from "react";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { time } from "console";
+
+export default function Redirect(){
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    useEffect(() => {
+        const params = searchParams.toString();
+        console.log("this is params");
+        if (params) {
+        localStorage.setItem("access_token", params);
+        router.push('/');
+        }
+    }, [router, searchParams]);
+
+    return(
+    <>
+    <div className="max-w-[1200px] flex flex-col justify-center items-center mx-auto">
+        <h1>This is the Redirect Page</h1>
+    </div>
+    </>
+    );
+}
