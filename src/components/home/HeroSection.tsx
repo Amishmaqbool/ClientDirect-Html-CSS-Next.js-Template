614fe8375e09fa05fdfa7176be4499099f8b097c
Amishmaqbool <amishmaqbool@gmail.com>
2024-10-25 03:29:42 +0500

    Feedback fixes

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -168,7 +168,7 @@ export default function HeroSection({
         </div>
 
         {isHomePage && (
-          <div className="max-w-[1280px] mx-auto px-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center items-center -mt-24 gap-4 md:gap-6">
+          <div className="max-w-[1280px] mx-auto px-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center items-center max-sm:-mt-12 -mt-24 gap-4 md:gap-6">
             {cardsData.map((card, index) => (
               <div
                 key={index}
