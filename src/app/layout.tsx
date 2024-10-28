a91819aa969acad428d6257d67fcc878465859b4
Amishmaqbool <amishmaqbool@gmail.com>
2024-10-28 23:40:59 +0500

    Ui feedback fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,6 +70,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <Script
           id="gtm-script"
           strategy="afterInteractive"
